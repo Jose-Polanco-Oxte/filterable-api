602a3c680a6a0331e97ebd84b805fd1e6e73960e
Antonio <josepolanco4569@gmail.com>
2025-08-26 17:10:55 -0600

    Refactor build configuration to streamline plugin management and enhance publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,58 @@
 plugins {
-    java
-    `java-library`
+    id("java")
+    id("org.jreleaser") version "1.19.0"
+    id("maven-publish")
+    id("signing")
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+allprojects {
+    group = "io.github.jose-polanco-oxte"
+    version = "1.0.0"
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+
+                pom {
+                    name.set(project.name)
+                    description.set("Módulo ${project.name} de mi librería")
+                    url.set("https://github.com/Jose-Polanco-Oxte/filterable-api")
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("jose-polanco-oxte")
+                            name.set("José Antonio Polanco Oxté")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/Jose-Polanco-Oxte/filterable-api")
+                        connection.set("scm:git:git://github.com/Jose-Polanco-Oxte/filterable-api.git")
+                        developerConnection.set("scm:git:ssh://github.com/Jose-Polanco-Oxte/filterable-api.git")
+                    }
+                }
+            }
+        }
+        signing {
+            sign(publishing.publications["mavenJava"])
+        }
     }
 }
 
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,18 +1,8 @@
 plugins {
-    java
-    `java-library`
 }
 
 repositories {
     mavenCentral()
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-    withJavadocJar()
-    withSourcesJar()
 }
 
 dependencies {
